8a2f678c781d221d1effd5f0d0deba2d08742dda
Jennifer C <jmcornejo-benitez@uh.edu>
2020-10-20 19:51:51 +0000

    Grocery Store

--- ex3.rb
+++ ex3.rb
@@ -7,3 +7,7 @@
 # git commit -am "blah blah"
 # git push
 # check repository
+
+
+# rails s to open my website
+# go into team corn then meme_website then rails s and preview -> preview running app.
